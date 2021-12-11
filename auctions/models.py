1657c93efda653f5ea9745d6fe344aab6a2e1cc9
boshhy <boshhy101@yahoo.com>
2021-12-10 17:24:31 -0800

    added some empty models

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,24 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class auction_listings(models.Model):
+    # title
+    # description
+    # starting_bid
+    # current_bid
+    # category
+    pass
+
+
+class watch_list(models.Model):
+    pass
+
+
+class bids(models.Model):
+    pass
+
+
+class comments(models.Model):
+    pass
